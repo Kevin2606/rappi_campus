674228af3589ded2535960d2e83fef90ae7771a1
gersonhdz8 <gersonhdz8@gmail.com>
2023-08-25 17:16:24 -0500

    feat: :sparkles: Se agrega Modelo de clientes
    Se implementan los metodos staticos y la clase ClienteModel

---
+++ model/cliente.js
@@ -0,0 +1,83 @@
+import connect from "../db/connectDB.js";
+
+const db = await connect().collection("clientes");
+export class ClienteModel{
+
+    constructor(){
+
+    }
+
+    static async createClient(cliente)
+    {
+        try {            
+            const newClient= await db.insertOne(cliente)
+            return newClient
+            //return console.log("Cliente Creado");
+        } 
+        catch (error) {
+            if (error.code === 11000)
+            return {
+                status: 400,
+                message: `${Object.keys(error.keyValue)[0]}: Ya está en uso`,
+            };
+        return { status: 400, message: "Error en la creación de usuario" };            
+        }
+        
+        
+    }
+    static async deleteClient(id_cliente)
+    {   
+        try {
+
+            const removeClient= await db.deleteOne(id_cliente)
+            console.log("Cliente eliminado correctamente");
+            return removeClient   
+        } 
+        catch (error) {
+            
+            return { status: 400, message: "Error al eliminar el usuario" };  
+            
+        }       
+        
+    }
+    static async updateClient(id,dataUpdateClient)
+    {
+        try {
+            const query={}
+            const updateClient= await db.updateOne(
+                {id_cliente:id},
+                {$set:dataUpdateClient}
+                );
+            console.log("Datos actualizados correctamente");
+            return updateClient   
+        } 
+        catch (error) {
+            
+            return { status: 400, message: "Error al actualizar el usuario" };  
+            
+        } 
+        
+    }
+    static async getClient(id)
+    {
+        try {
+            const getClient=await db.findOne({ id_cliente: id });
+            return getClient
+        } catch (error) {
+            return { status: 400, message: "Usuario no encontrado" };
+        }
+    }
+
+    static async getAllClient()
+    {
+        try {
+            const getClients=await db.find();
+            return getClients
+        } catch (error) {
+            return { status: 400, message: error.message };
+        }
+        
+    }
+
+    
+}
