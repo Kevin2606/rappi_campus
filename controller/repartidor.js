e1a08af2e3b2cc446a8b467f821bc461342869fc
Kevin Gallardo <andreskevin2606@gmail.com>
2023-08-25 16:29:00 -0500

    feat: :sparkles: Se implemento controlador y rutas de repartidores

---
+++ controller/repartidor.js
@@ -0,0 +1,64 @@
+import RepartidorModel from '../models/repartidor';
+
+export default class RepartidorController {
+    static async obtenerRepartidores(req, res) {
+        try {
+            const repartidores = await RepartidorModel.obtenerRepartidores();
+            res.status(200).json(repartidores);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+    static async obtenerRepartidor(req, res) {
+        try {
+            const repartidor = await RepartidorModel.obtenerRepartidor(req.params.id);
+            res.status(200).json(repartidor);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+    static async crearRepartidor(req, res) {
+        try {
+            const repartidor = await RepartidorModel.crearRepartidor(req.body);
+            res.status(200).json(repartidor);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+
+    static async actualizarRepartidor(req, res) {
+        try {
+            const repartidor = await RepartidorModel.actualizarRepartidor(req.params.id, req.body);
+            res.status(200).json(repartidor);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+
+    static async eliminarRepartidor(req, res) {
+        try {
+            const repartidor = await RepartidorModel.eliminarRepartidor(req.params.id);
+            res.status(200).json(repartidor);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+
+    static async obtenerRepartidorPorCedula(req, res) {
+        try {
+            const repartidor = await RepartidorModel.obtenerRepartidorPorCedula(req.params.cedula);
+            res.status(200).json(repartidor);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+
+    static async obtenerRepartidorPorCorreo(req, res) {
+        try {
+            const repartidor = await RepartidorModel.obtenerRepartidorPorCorreo(req.params.correo);
+            res.status(200).json(repartidor);
+        } catch (error) {
+            res.status(error.status).json({ message: error.message });
+        }
+    }
+}
