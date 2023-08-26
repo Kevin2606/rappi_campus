e4a793472a1460f4fd3171537b98481b2d920b42
Kevin2606 <andreskevin2606@gmail.com>
2023-08-26 01:04:44 -0500

    feat: :sparkles: Creacion de archivo de peticiones de prueba Repartidores

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import repartidoresRouter from './routes/repartidores.js';
+import pedidosRouter from './routes/pedidos.js';
 
 dotenv.config();
 
 const app = express();
-
-app.get('/repartidores', repartidoresRouter);
+app.use(express.json());
+app.use('/repartidores', repartidoresRouter);
+app.use('/pedidos', pedidosRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on http://localhost:${process.env.PORT}`);
