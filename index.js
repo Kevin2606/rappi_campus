6a5ebd1e3e51e80d42bf5985ebd3a56dbd5d27dc
Kevin G <54305330+Kevin2606@users.noreply.github.com>
2023-08-26 20:27:14 -0500

    feat: :sparkles: Cambios para el modulo de Restaurantes (#14)
    Se añade cambios para el modulo de restaurantes: Controlador, Rutas y peticiones de prueba http

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import repartidoresRouter from './routes/repartidores.js';
 import pedidosRouter from './routes/pedidos.js';
+import restaurantesRouter from './routes/restaurantes.js';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use('/repartidores', repartidoresRouter);
 app.use('/pedidos', pedidosRouter)
+app.use('/restaurantes', restaurantesRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on http://localhost:${process.env.PORT}`);
