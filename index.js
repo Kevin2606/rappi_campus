7412982205b0926b97a5eb55374ccea3885c1fc8
Kevin2606 <andreskevin2606@gmail.com>
2023-08-28 21:33:56 -0500

    feat: :sparkles: Se implementa autorizacion por roles

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ import pedidosRouter from './routes/pedidos.js';
 import restaurantesRouter from './routes/restaurantes.js';
 import clientesRouter from './routes/clientes.js';
 import productosRouter from './routes/productos.js';
+import { config as configAuth, authorize } from './config/auth.js';
+
 dotenv.config();
+configAuth();
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`Request ${req.method} ${req.url}`);
+    req.user = { rol : 'admin'}
+    next();
+})
+app.use(authorize);
 app.use('/repartidores', repartidoresRouter);
 app.use('/pedidos', pedidosRouter)
 app.use('/restaurantes', restaurantesRouter);
