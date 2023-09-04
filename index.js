1fbb001d36b2f8cb3f4c05a5fd5d9a10093ec797
GersonHdz <gersonhdz8@gmail.com>
2023-09-04 05:44:25 -0500

    feat: :sparkles: Se implementa manejo de versiones(api_version)
    Se agrega una carpeta nueva y un archivo en dónde se manejan las diferentes versiones para cada modulo

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import repartidoresRouter from './routes/repartidores.js';
-import pedidosRouter from './routes/pedidos.js';
-import restaurantesRouter from './routes/restaurantes.js';
-import clientesRouter from './routes/clientes.js';
-import productosRouter from './routes/productos.js';
 import authRouter from './routes/auth.js';
 import passportConfig from './config/passportConfig.js';
 import { config as configAuth, authorize } from './config/auth.js';
+import { versionedClientesRouter,versionedPedidosRouter,versionedProductosRouter,versionedRepartidoresRouter,versionedRestaurantesRouter } from './api/api_version.js';
 import errorHandler from "./utils/errorHandler.js";
 
 dotenv.config();
@@ -19,11 +15,11 @@ app.use(express.json());
 app.use('/auth', authRouter)
 app.use(passportConfig.initialize())
 app.use(passportConfig.authenticate('bearer', { session: false }), authorize)
-app.use('/repartidores', repartidoresRouter);
-app.use('/pedidos', pedidosRouter)
-app.use('/restaurantes', restaurantesRouter);
-app.use('/clientes', clientesRouter);
-app.use('/productos', productosRouter);
+app.use('/repartidores', versionedRepartidoresRouter);
+app.use('/pedidos', versionedPedidosRouter)
+app.use('/restaurantes', versionedRestaurantesRouter);
+app.use('/clientes', versionedClientesRouter);
+app.use('/productos', versionedProductosRouter);
 
 app.use(errorHandler);
 
